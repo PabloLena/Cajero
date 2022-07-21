afd908721bfe39f8d8841c73b77b532a40c0e4e3
pablo <pablo@DESKTOP-JSUKNPB>
2022-07-21 18:32:50 -0300

    Add project files.

---
+++ WebApplication1/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entidades;
+
+namespace WebApplication1
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
---
+++ WebApplication1/Controllers/GenerosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entidades;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/generos")]
+    public class GenerosController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public GenerosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Genero>>> Get() {
+            return await context.Generos.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Genero>> Get(int id)
+        {
+            var entidad = await context.Generos.FirstOrDefaultAsync(p => p.Id == id);
+            if (entidad == null) return NotFound();
+            return entidad;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Genero genero) {
+
+            context.Add(genero);
+            await context.SaveChangesAsync();
+
+
+            return Ok();
+        }
+    }
+}
